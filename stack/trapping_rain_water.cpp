f6615c0719762355f317e217ab93c5b5a19a5f79
Pengcheng Wan <kevinwan1991@gmail.com>
2019-01-22 08:32:49 -0500

    Update trapping_rain_water.cpp

--- stack/trapping_rain_water.cpp
+++ stack/trapping_rain_water.cpp
@@ -1,20 +1,31 @@
 class Solution
 {
-public: int trap(const vector<int>& A) {
+public:
+    int trap(const vector<int>& A) 
+    {
         if (A==null) return 0;
         std::stack<int> s;
         int i = 0, maxWater = 0, maxBotWater = 0;
-        while (i < A.length){
-            if (s.isEmpty() || A[i]<=A[s.peek()]){
+        while (i < A.length)
+        {
+            if (s.isEmpty() || A[i]<=A[s.peek()])
+            {
                 s.push(i++);
             }
-            else {
+            else
+            {
                 int bottom = s.pop();
-                maxBottomWater = s.isEmpty()? // empty means no il
-                0:(Math.min(A[s.peek()],A[i])-A[bottom])*(i-s.peek()-1);
+                maxBottomWater = s.isEmpty()?
+                0
+                :
+                (Math.min(A[s.peek()],A[i])-A[bottom])*(i-s.peek()-1);
                 maxWater += maxBotWater;
             }
         }
         return maxWater;
     }
 }
+/*
+** water will be trapped when a valley (v shape) exists, a the total size can be added up level by level.
+** we need a stack to keep track of the desending (left) side of the valley. 
+*/
