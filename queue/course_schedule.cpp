5fe79095ddcc2e0eb59f4c3cc2f4aeebdfd8ef4c
Pengcheng Wan <kevinwan1991@gmail.com>
2019-02-04 12:06:34 -0500

    Update course_schedule.cpp

--- queue/course_schedule.cpp
+++ queue/course_schedule.cpp
@@ -1,3 +1,4 @@
+    // BFS
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) 
     {
         vector<unordered_set<int>> graph(numCourses);
@@ -26,4 +27,31 @@
             }
         }
         return counter == numCourses;
+    }
+
+
+    // DFS
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+    {
+        vector<unordered_set<int>> matrix(numCourses); // save this directed graph
+        for(int i = 0; i < prerequisites.size(); ++ i)
+            matrix[prerequisites[i][1]].insert(prerequisites[i][0]);
+
+        unordered_set<int> visited;
+        vector<bool> flag(numCourses, false);
+        for(int i = 0; i < numCourses; ++ i)
+            if(!flag[i])
+                if(DFS(matrix, visited, i, flag))
+                    return false;
+        return true;
+    }
+    bool DFS(vector<unordered_set<int>> &matrix, unordered_set<int> &visited, int b, vector<bool> &flag)
+    {
+        flag[b] = true;
+        visited.insert(b);
+        for(auto it = matrix[b].begin(); it != matrix[b].end(); ++ it)
+            if(visited.find(*it) != visited.end() || DFS(matrix, visited, *it, flag))
+                return true;
+        visited.erase(b);
+        return false;
     }
