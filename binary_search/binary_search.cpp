9bfff4e4820d2887ab0a24e0047279954646f9ce
Pengcheng Wan <kevinwan1991@gmail.com>
2019-02-21 13:52:01 -0500

    Update binary_search.cpp

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -17,10 +17,10 @@ int upper_bound(const std::vector<int>& nums, int target)
     while(left < right)
     {
         int mid = (right - left) / 2 + left ;
-        if(nums[mid] > target) 
+        if (nums[mid] <= target)
+            left = mid + 1;             
+        else         
             right = mid ;
-        else 
-            left = mid + 1; 
     }
     return left;
 }
