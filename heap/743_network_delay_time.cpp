0e1618d0f2acf7e3a88d9404b11820c6c28e1aea
Pengcheng Wan <kevinwan1991@gmail.com>
2019-02-23 12:16:06 -0500

    Update 743_network_delay_time.cpp

--- heap/743_network_delay_time.cpp
+++ heap/743_network_delay_time.cpp
@@ -14,24 +14,24 @@ public:
         priority_queue<pii, vector<pii>, greater<pii> > pq;
         pq.push(make_pair(0, k));
         	    
-		int u, v, w;	    
+	int u, v, w;	    
         vector<bool> vis(n + 1, false);
         while (!pq.empty()) 
         {
             pii p = pq.top(); pq.pop();
             u = p.second;
-	    	if (vis[u]) continue; // longer path in priority queue for the same node will be disgarded.
+	    if (vis[u]) continue; // longer path in priority queue for the same node will be disgarded.
             
-			for (auto& to : g[u]) 
-			{
-				v = to.first, w = to.second;
-				if (dist[v] > dist[u] + w) 
-				{
-					dist[v] = dist[u] + w;
-					pq.push(make_pair(dist[v], v));
-				}
-			}
-			vis[u] = true;
+	    for (auto& to : g[u]) 
+	    {
+		v = to.first, w = to.second;
+		if (dist[v] > dist[u] + w) 
+		{
+			dist[v] = dist[u] + w;
+			pq.push(make_pair(dist[v], v));
+		}
+	    }
+	    vis[u] = true;
         }
         int ans = *max_element(dist.begin() + 1, dist.end());
         return ans == inf ? -1 : ans;
