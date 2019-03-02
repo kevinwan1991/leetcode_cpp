77bbb79c6b48ea0b462051e176643e5b0ef93fed
Pengcheng Wan <kevinwan1991@gmail.com>
2019-03-02 18:07:34 -0500

    Update 939_minimum_area_rectangle.cpp

--- hashmap/939_minimum_area_rectangle.cpp
+++ hashmap/939_minimum_area_rectangle.cpp
@@ -13,7 +13,7 @@ int minAreaRect(vector<vector<int>>& points)
     for (int i = 0; i < points.size(); i++) 
     {
         vector<int> &p1 = points[i];
-        for (int j = i+1; j < points.size(); j++) //pair(p1,p2) == pair(p2,p1), so j = i+1;
+        for (int j = i+1; j < points.size(); j++) //diagnal(p1,p2) == diagnal(p2,p1), so j = i+1;
         {
             vector<int> &p2 = points[j];
             if (p1[0] != p2[0] && p1[1] != p2[1] 
